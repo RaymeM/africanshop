3c5cf7b22eba076e87523b38377f49ae072a75ba
A Remi M <93027585+RaymeM@users.noreply.github.com>
2022-05-30 14:52:11 +0200

    Besslan changes

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+require("dotenv/config");
+require("./db")
 const express = require('express');
-
 const app = express()
 const port = process.env.PORT || 3000;
 
 const shopsRoute = require('./routes/shops');
-//const commentsRoute = require('./routes/comments');
+
+
 
 
 app.use(express.json());
 
 app.use("/shops", shopsRoute);
-//app.use("/comments", commentsRoute);
+
 
 
 
