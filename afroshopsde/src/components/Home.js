7eaafcf2bafe0c37f62a1e7942c878529772b70d
A Remi M <93027585+RaymeM@users.noreply.github.com>
2022-06-07 21:21:10 +0200

    styles1

--- afroshopsde/src/components/Home.js
+++ afroshopsde/src/components/Home.js
@@ -1,8 +1,15 @@
 function Home() {
   return (
     <div id="home">
-      <h1>HOME PAGE!!!</h1>
-      <image className="firstPicture"></image>
+      <h1>AFROSHOP.de</h1>
+      <div className="container">
+        <div className="container-child">
+          <img src="afroshopsde\public\photos\Afroshop_in_Aachen.JPG" />,
+        </div>
+        <div className="container-child">2</div>
+        <div className="container-child">3</div>
+        <div className="container-child">4</div>
+      </div>
     </div>
   );
 }
--- afroshopsde/src/components/Map.js
+++ afroshopsde/src/components/Map.js
@@ -3,6 +3,8 @@ import "../App.css";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import Search from "../components/Search";
+import SearchLeaflet from "../components/SearchLeaflet";
+
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -13,11 +15,96 @@ L.Icon.Default.mergeOptions({
 });
 
 export default function Map() {
-  const position = [52.520005, 13.404954];
+ 
+  const position = [51.37638483311173, 7.696039042002252];
+  
+
+  const posObj = [
+      {
+        name: "Afroland",
+        lat: 51.37601901111738,
+        lng: 7.694696698597307,
+        email: "afroland@gmail.com",
+        phone: "0176 75202144",
+        detail: "General African products"
+      },
+      {
+        name: "Friseur Hairtrends",
+        lat: 51.37347350931192,
+        lng: 7.703573827433566,
+        email: "-",
+        phone: "02371 788730",
+        detail: "Hair salon"
+      },
+      {
+        name: "Afro Shop Becky",
+        lat: 51.359387791380534,  
+        lng: 7.4612658587855,
+        email: "-",
+        phone: "02331 3067003",
+        detail: "Hair salon"
+      },
+      {
+        name: "Afro World Dortmund",
+        lat: 51.52413481709894, 
+        lng: 7.480039130421862,
+        email: "-",
+        phone: "0231 4759235",
+        detail: "African goods store"
+      },
+      {
+        name: "AfroShop Fouta",
+        lat: 51.52263494242923,
+        lng:  7.477262530019894, 
+        email: "-",
+        phone: "0231 53098523",
+        detail: "African goods store"
+      },
+      {
+        name: "Alpha and Omega International Afro Shop",
+        lat: 52.48831290524142, 
+        lng: 13.426293642506666,
+        email: "-",
+        phone: "030 61624972",
+        detail: "Wholesaler of African goods"
+      },
+      {
+        name: "Max Afro Shop",
+        lat: 52.48495715345721, 
+        lng:  13.428293040660224, 
+        email: "-",
+        phone: "030 53606308",
+        detail: "African goods store"
+      },
+      {
+        name: "Miss Selli's Afroshop",
+        lat: 52.54908896014544, 
+        lng:  13.349914056002838, 
+        email: "-",
+        phone: "030 32594395",
+        detail: "Beauty salon"
+      },
+      {
+        name: "Asia & Afroshop",
+        lat: 52.545812675161805, 
+        lng:  13.35092105194194, 
+        email: "-",
+        phone: "030 62708505",
+        detail: "African goods store / Beauty salon"
+      },
+      {
+        name: "Exo-markt Afro Shop",
+        lat: 52.567382676232405, 
+        lng:  13.329404940662549,
+        email: "-",
+        phone: "030 95625817",
+        detail: "general store"
+      },
+  ]
 
   return (
     <>
-      <Search />
+      
       <div className="leaflet-container">
         <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
           <TileLayer
@@ -29,6 +116,24 @@ export default function Map() {
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
+          {/* {allPositions.map(pos => (
+            <Marker position={}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+          ) ) } */}
+          {posObj.map((pos) => (
+            <Marker position={[pos.lat, pos.lng]}>
+            <Popup>
+              <h3>{pos.name}</h3>
+              <h4>{pos.detail}</h4>
+              <h4>{pos.email}</h4>
+              <h4>{pos.phone}</h4>
+            </Popup>
+          </Marker>
+          ))}
+          <SearchLeaflet/>
         </MapContainer>
       </div>
     </>
